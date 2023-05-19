c3922067ff5bfc0479eb1e9bc3dfcbe1a3e5fd1c
KeoCode <keocode101@gmail.com>
2023-05-19 18:00:34 +0000

    intital commit

---
+++ skfitness/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for skfitness project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'skfitness.settings')
+
+application = get_asgi_application()
