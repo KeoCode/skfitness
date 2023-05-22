51bc23aa0cad2c9da97fb238d7696abebef9a827
KeoCode <keocode101@gmail.com>
2023-05-22 18:31:28 +0000

    Added category filtering

--- packages/views.py
+++ packages/views.py
@@ -7,9 +7,29 @@ def all_packages(request):
     """ A view to show all packages, including sorting and search queries """
 
     packages = Packages.objects.all()
+    query = None
+    categories = None
+
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            packages = packages.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('packages'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            packages = packages.filter(queries)
+
 
     context = {
         'packages': packages,
+        'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'packages/packages.html', context)
