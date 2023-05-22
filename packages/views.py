0537f76e8704a82bdf007e4eb0d4982ac69c76cf
KeoCode <keocode101@gmail.com>
2023-05-22 15:16:34 +0000

    Added packages views and templates

--- packages/views.py
+++ packages/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Packages
 
 # Create your views here.
+
+def all_packages(request):
+    """ A view to show all packages, including sorting and search queries """
+
+    packages = Packages.objects.all()
+
+    context = {
+        'packages': packages,
+    }
+
+    return render(request, 'packages/packages.html', context)
