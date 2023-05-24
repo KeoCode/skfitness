4224275d509b9695f0d523e945de752308b4c59a
KeoCode <keocode101@gmail.com>
2023-05-24 20:16:08 +0000

    added add to bag functionality

--- bag/views.py
+++ bag/views.py
@@ -13,14 +13,19 @@ def view_bag(request):
 
 
 def add_to_bag(request, item_id):
-    """ Add a quantity of the specified packages to the shopping bag """
+    """ Add a quantity of the specified product to the shopping bag """
 
-    packages = get_object_or_404(Package, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    messages.success(request, f'Added {packages.name} to your bag')
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity
 
     request.session['bag'] = bag
+    print(request.session['bag'])
     return redirect(redirect_url)
 
 
