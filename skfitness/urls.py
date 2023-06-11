54fae25b69e0ff0caa5bec5788d8c0fcf2ec8e35
KeoCode <keocode101@gmail.com>
2023-06-11 21:35:14 +0000

    Added blog templates and widget tempalte

--- skfitness/urls.py
+++ skfitness/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('summernote/', include('django_summernote.urls')),
     path('', include('home.urls')),
     path('packages/', include('packages.urls')),
     path('bag/', include('bag.urls')),
