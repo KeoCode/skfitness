29823b1b26ce6e50049d671b6e46afa16aa2c14f
KeoCode <keocode101@gmail.com>
2023-06-11 17:44:27 +0000

    Added custom 404 page

--- packages/urls.py
+++ packages/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .views import handler404
 
 urlpatterns = [
     path('', views.all_packages, name='packages'),
@@ -7,4 +8,5 @@ urlpatterns = [
     path('add/', views.add_package, name='add_package'),
     path('edit/<int:package_id>/', views.edit_package, name='edit_package'),
     path('delete/<int:package_id>/', views.delete_package, name='delete_package'),
+    handler404 = 'skfitness.views.handler404'
 ]
