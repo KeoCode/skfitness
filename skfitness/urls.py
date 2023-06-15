1043eb7f1c29178cec262c234a88fc287e252e18
KeoCode <keocode101@gmail.com>
2023-06-15 20:04:50 +0000

    last little changes eg external rel link

--- skfitness/urls.py
+++ skfitness/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -32,4 +31,3 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-handler404 = 'skfitness.views.handler404'
